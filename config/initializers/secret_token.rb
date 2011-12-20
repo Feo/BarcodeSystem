655a3a5899c939e581b0b61893f502124828e7cd
feo <jjzxcc@gmail.com>
2011-12-20 09:55:53 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barcodesystem::Application.config.secret_token = '07ca2ce1c529dfdbc0eb99eb59f09e80bdbcc14fe21251d93671272453099feaa4826c7697db43d65308bb606a973ad54bd3ee41c3f090fd21cecb0a6230ed32'
